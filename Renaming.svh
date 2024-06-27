1bfba31991e3efba053181b06328ecf5a5b23dfa
jennielien <115868792+jennielien@users.noreply.github.com>
2024-06-28 02:54:27 +0800

    Update Renaming.svh

--- Renaming.svh
+++ Renaming.svh
@@ -6,16 +6,16 @@
 `define REG_ADDR_LEN 5
 
 typedef struct packed {
-    logic unsigned [REG_ADDR_LEN:0] src1;
-    logic unsigned [REG_ADDR_LEN:0] src2;
-    logic unsigned [REG_ADDR_LEN:0] dest;
+    logic unsigned [`REG_ADDR_LEN-1:0] src1;
+    logic unsigned [`REG_ADDR_LEN-1:0] src2;
+    logic unsigned [`REG_ADDR_LEN-1:0] dest;
 } ARCH_REG;
 
 typedef struct packed {
-    logic unsigned [REG_ADDR_LEN:0] src1;
-    logic unsigned [REG_ADDR_LEN:0] src2;
-    logic unsigned [REG_ADDR_LEN:0] dest;
-    logic unsigned [REG_ADDR_LEN:0] dest_old;
+    logic unsigned [`REG_ADDR_LEN-1:0] src1;
+    logic unsigned [`REG_ADDR_LEN-1:0] src2;
+    logic unsigned [`REG_ADDR_LEN-1:0] dest;
+    logic unsigned [`REG_ADDR_LEN-1:0] dest_old;
 } PHYS_REG;
 
-`endif _RENAMING_
+`endif // _RENAMING_
