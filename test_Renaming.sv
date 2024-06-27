b11d4d1d3c086c0712f8b4b310e56bcf0fcee4b4
MengtingTu <mtt@umich.edu>
2024-06-28 02:16:47 +0800

    modify renaming testbench

--- test_Renaming.sv
+++ test_Renaming.sv
@@ -36,21 +36,22 @@ module test_Renaming();
     @(negedge clk);
   endtask
 
-  task test_on_commit(input task_commit, task_commit_phys_reg)
+  // Test task for commit operation
+  task test_on_commit(input logic task_commit_flag, input logic [4:0] task_commit_phys_reg);
     reset = 1'b1;
     assign_flag = 1'b0;
-    commit = is_commit;
-    arch_reg = 0;
+    commit_flag = task_commit_flag;
+    arch_reg = '{src1: 5'b0, src2: 5'b0, dest: 5'b0};
     commit_phys_reg = task_commit_phys_reg;
     @(negedge clk);
     reset = 1'b0;
     wait_until_done();
   endtask
 
-  task test_on_arch(input task_arch_reg)
+  task test_on_arch(input ARCH_REG task_arch_reg)
     reset = 1'b1;
     assign_flag = 1'b0;
-    commit = 1'b0;
+    commit_flag = 1'b0;
     arch_reg = task_arch_reg;
     commit_phys_reg = 0;
     @(negedge clk);
@@ -58,10 +59,10 @@ module test_Renaming();
     wait_until_done();
   endtask
 
-  task test_on_assign(input test_assign)
+  task test_on_assign(input logic test_assign)
     reset = 1'b1;
     assign_flag = test_assign;
-    commit = 1'b0;
+    commit_flag = 1'b0;
     arch_reg = 0;
     commit_phys_reg = 0;
     @(negedge clk);
@@ -81,7 +82,35 @@ module test_Renaming();
 
     @(negedge clk);
     #1000 reset = 1'b1;
+    
+    // Test committing physical registers back to the free list
+    test_on_commit(1'b1, 5'd1);
+    test_on_commit(1'b1, 5'd2);
 
+
+    // Test assigning physical registers to architectural registers
+    // Read after write (true dependency)
+    test_on_arch('{src1: 5'd1, src2: 5'd2, dest: 5'd3});
+    test_on_arch('{src1: 5'd3, src2: 5'd4, dest: 5'd5});
+
+    // Write after write (false dependency)
+    test_on_arch('{src1: 5'd1, src2: 5'd2, dest: 5'd3});
+    test_on_arch('{src1: 5'd4, src2: 5'd5, dest: 5'd3});
+
+    // Write after read (false dependency)
+    test_on_arch('{src1: 5'd1, src2: 5'd2, dest: 5'd3});
+    test_on_arch('{src1: 5'd4, src2: 5'd5, dest: 5'd1});
+
+    @(negedge clk);
+    #1000 reset = 1'b1;
+
+    // Randomized testing, free list empty and full
+    for (int i = 0; i < 33; i++) begin
+        ARCH_REG rand_arch_reg = '{src1: $urandom_range(0, 31), src2: $urandom_range(0, 31), dest: $urandom_range(0, 31)};
+        test_on_arch(rand_arch_reg);
+    end
+
+    
     reset = 1'b1;
     quit  = 1'b0;
     // while (~quit) begin
