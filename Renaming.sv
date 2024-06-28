63e77437ce5541edbb5104f5605c6a4c4775e426
jennielien <115868792+jennielien@users.noreply.github.com>
2024-06-28 13:54:12 +0800

    Update Renaming.sv

--- Renaming.sv
+++ Renaming.sv
@@ -3,20 +3,20 @@
 module RegisterRenaming (
     input logic clk,
     input logic reset,
-    // input logic en,
     input ARCH_REG arch_reg,
     input logic commit_flag, //from ROB
     input logic assign_flag, // form decoder
     input logic [4:0] commit_phys_reg,
-    output PHYS_REG phys_reg
+    output PHYS_REG phys_reg //output logic PHYS_REG phys_reg
     // output done
 );
 
     logic [4:0] assign_dest_reg;
-//    logic freelist_inst_done;
-    logic [4:0] rat_phys_reg;
+    //    logic freelist_inst_done;
+    PHYS_REG rat_phys_reg;
     
     assign phys_reg = (arch_reg.dest == 5'd0) ? 5'd0 : rat_phys_reg;
+    //assign phys_reg = rat_phys_reg;
     
     RAT rat_inst(
         .clk(clk),
@@ -53,24 +53,20 @@ module RAT (
 );
 
     logic [(`REG_LEN*`REG_ADDR_LEN-1):0] map_curr, map_next, map_on_reset;
-    logic [4:0] old_phys_reg_dest;
+//    logic [4:0] old_phys_reg_dest;
     
     // ARCH_REG arch_reg_reg;
-    // always_ff @(posedge clock) begin
-    //     unique if (en) begin 
-    //         arch_reg_reg <= arch_reg;
-    //         done <= 1'b0;
-    //     end else begin 
-    //         arch_reg_reg <= arch_reg_reg;
-    //         done <= done;
-    //     end
-    // end
+    logic [4:0] assign_reg_curr;
+     always_ff @(posedge clk) begin
+         assign_reg_curr <= assign_reg;
+     end
     
     always_ff @(posedge clk) begin
         unique if (reset) begin
             map_curr <= map_on_reset;
             // done <= 1'b0;
-        end else begin
+        end 
+        else begin
             map_curr <= map_next;
             // done <= 1'b1;
         end 
@@ -80,12 +76,15 @@ module RAT (
     assign map_on_reset = 1'b0 << `REG_LEN*`REG_ADDR_LEN; 
 
     always_comb begin
-        phys_reg.src1 = map_curr >> (arch_reg.src1*`REG_ADDR_LEN);
-        phys_reg.src2 = map_curr >> (arch_reg.src2*`REG_ADDR_LEN);
-        phys_reg.dest_old = map_curr >> (arch_reg.dest*`REG_ADDR_LEN);
         map_next = map_curr;
-        if (assign_flag) map_next = map_next | (assign_reg << arch_reg.dest*`REG_ADDR_LEN);
+        if (assign_flag) map_next = map_next | (assign_reg_curr << arch_reg.dest*`REG_ADDR_LEN);
     end
+    
+    assign phys_reg.src1 = map_curr >> (arch_reg.src1*`REG_ADDR_LEN);
+    assign phys_reg.src2 = map_curr >> (arch_reg.src2*`REG_ADDR_LEN);
+//    assign phys_reg.dest = (assign_reg == 5'd0) ? 5'd0 : assign_reg;
+    assign phys_reg.dest = assign_reg_curr;
+    assign phys_reg.dest_old = map_curr >> (arch_reg.dest*`REG_ADDR_LEN);
 
 endmodule 
 
@@ -103,6 +102,8 @@ module FreeList (
     logic [4:0] free_list [30:0];
     logic [4:0] free_list_front; //assign
     logic [4:0] free_list_end; //return
+    
+    logic [4:0] free_list_front_curr;
 
     //assign phys_reg for new instruction and return phys_reg from ROB
     always_ff @(posedge clk) begin
@@ -117,6 +118,8 @@ module FreeList (
         else begin
             if (assign_flag) begin
             free_list_front <= (free_list_front + 1) % 31;
+            free_list_front_curr <= free_list_front;
+            //assign_reg = free_list[free_list_front];
             end
             if (return_flag) begin
             free_list[(free_list_end + 1) % 31] <= return_reg;
@@ -125,7 +128,7 @@ module FreeList (
             // done <= 1'b1;
         end
     end
-
-    assign assign_reg = (assign_flag ? free_list[free_list_front] : 5'b00000);
+    
+assign assign_reg = assign_flag ? free_list[free_list_front_curr] : 5'b00000;
         
 endmodule
