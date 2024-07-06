723145760bfd2a2d4438ca1a7fe6aaf5de4045c8
jennielien <115868792+jennielien@users.noreply.github.com>
2024-07-06 21:19:37 +0800

    Update map.svh

--- include/map.svh
+++ include/map.svh
@@ -2,11 +2,10 @@
 `define _MAP_
 
 `define REG_LEN 32
-`define REG_WIDTH 32
 `define REG_ADDR_LEN 5
 
 `define ROB_SIZE 64
-`define ROB_ADDR_LEN 6
+`define ROB_TAG_LEN 6
 
 typedef struct packed {
     logic unsigned [`REG_ADDR_LEN-1:0] src1;
@@ -17,17 +16,19 @@ typedef struct packed {
 typedef struct packed {
     logic [1:0] data_stat; // data status: 00 = in regfile, 10 = in ROB (not ready), 11 = in ROB (ready)
     logic [`REG_ADDR_LEN-1:0] reg_addr; // register address
-    logic [`ROB_ADDR_LEN-1:0] rob_tag; // ROB entry
+    logic [`ROB_TAG_LEN-1:0] rob_tag; // ROB entry
 } RENAMED_REG;
 
 typedef struct packed {
     RENAMED_REG src1;
     RENAMED_REG src2;
-} RENAMED_SRC;
+    logic [`REG_ADDR_LEN-1:0] dest;
+    logic [`ROB_TAG_LEN-1:0] rob_tag;
+} RENAMED_PACK;
 
 typedef struct packed {
-    logic [`ROB_ADDR_LEN-1:0] rob_tag;
+    logic [`ROB_TAG_LEN-1:0] rob_tag;
     logic ready_in_rob;
-} MAP_ENTRY
+} MAP_ENTRY;
 
 `endif // _MAP_
