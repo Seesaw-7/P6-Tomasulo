3388ab7facf0b6b7c7c23d7c36482cf73273c2d7
Seesaw <weiseesaw@gmail.com>
2024-07-08 14:52:15 +0200

    fix: RS in P6 style

--- include/reservation_station.svh
+++ include/reservation_station.svh
@@ -3,21 +3,20 @@
 
 // local header
 
+`include "map.svh"
 
 typedef struct packed {
-    ALU1_FUNC insn;
-    logic [`REG_ADDR_WIDTH-1:0] inp1;
-    logic [`REG_ADDR_WIDTH-1:0] inp2;
-    logic ready1;
-    logic ready2;
-    logic [`REG_ADDR_WIDTH-1:0] dst;
-    logic [`ENTRY_WIDTH-1:0] Bday;
-    logic valid; // whether the data in this entry can be used
-} RS_entry_t; // entry type for the issue queue ahead of ALU1
-
-// typedef struct packed {
-//     logic [REG_ADDR_WIDTH-1:0];
-//     logic ready;
-// } ready_bits_t;
+    ALU_FUNC                       func;    //TODO: revise later, decided by decoder and FU
+    // logic    [6:0]                 opcode;  //TODO: revise later, decided by decoder and FU
+    logic    [`ROB_TAG_LEN-1:0]     t1;
+    logic    [`ROB_TAG_LEN-1:0]     t2;
+    logic    [`ROB_TAG_LEN-1:0]     dst;
+    logic                           ready1;
+    logic                           ready2;
+    logic    [`XLEN-1:0]            v1;
+    logic    [`XLEN-1:0]            v2;
+    logic    [`ENTRY_WIDTH-1:0]     Bday;
+    logic                          valid;  // whether the data in this entry can be used
+} RS_ENTRY; // entry type for all RS
 
 `endif
--- src/reservation_station.sv
+++ src/reservation_station.sv
@@ -2,6 +2,9 @@
 
 `timescale 1ns/1ps
 
+//TODO: use CAM instead of loop to improve gate number
+//TODO: async accept wakeup value but sync update RS entries, so async judge insn_ready based on wakeup tag as well
+
 module reservation_station #(
     parameter NUM_ENTRIES = 4, // #entries in the reservation station
     parameter ENTRY_WIDTH = 2  // #entries = 2^{ENTRY_WIDTH}
@@ -10,31 +13,36 @@ module reservation_station #(
     input logic clk,
     input logic reset,
     input logic load, // whether we load in the instruction (assigned by dispatcher)
-    input logic issue, // whether the issue queue should output one instruction (assigned by issue unit)
-    input logic wakeup, // set by outside logic, indicating whether to set the ready bit of previously issued dst reg to Yes
+    input logic issue, // whether the issue queue should output one instruction (assigned by issue unit), should be stable during clock edge
+    input logic wakeup, // set by issue unit, indicating whether to set the ready tag of previously issued dst reg to Yes
+                        // this should better be set 1 cycle after issue exactly is the FU latency is one, should be stable during clock edge
 
     // input data
-    // input AL_FUNC insn, 
-    input DECODED_INST insn,
-    input logic [PHY_REG_ADDR_LEN-1:0] inp1, inp2, dst, // previous renaming unit ensures that dst != inp1 and dst != inp2
-    input logic wakeup_reg_addr, 
+    input ALU_FUNC func,
+    input logic [ROB_TAG_LEN-1:0] t1, t2, dst, // previous renaming unit ensures that dst != inp1 and dst != inp2
+    input ready1, ready2,
+    input logic [XLEN-1:0] v1, v2, 
+    input logic [ROB_TAG_LEN-1:0] wakeup_tag,
+    input logic [XLEN-1:0] wakeup_value, 
 
     // output signals
-    // output logic issue_ready, // indicates if an instruction is ready to be executed
     output logic insn_ready, // indicates if there exists an instruction that is ready to be issued
     output logic is_full, // all entries of the reservation station is occupied, cannot load in more inputs
 
     // output data
-    // output logic [NUM_ENTRIES-1:0] insn_out,
-    input DECODED_INST insn_out,
-    output logic [PHY_REG_ADDR_LEN-1:0] inp1_out, inp2_out, dst_out
+    output ALU_FUNC func_out,
+    output logic [XLEN-1:0] v1_out, v2_out,
+    output [`ROB_TAG_LEN-1:0] dst_tag
 );
 
+    logic [ROB_TAG_LEN-1:0] wakeup_tag_reg;
+    logic [XLEN-1:0] wakeup_value_reg;
 
-    // the only two internal storages that need to be updated synchronously
-    issue_queue_entry_t entries [NUM_ENTRIES];
-    logic [PHY_REG_NUM-1:0] ready_table;
+    assign wakeup_tag_reg = wakeup? wakeup_tag : wakeup_tag_reg; // in case values are not stable at clock edge
+    assign wakeup_value_reg = wakeup? wakeup_value : wakeup_value_reg; 
 
+    // the only internal storage that need to be updated synchronously
+    RS_ENTRY entries [NUM_ENTRIES];
 
     // internal signals below are all updated with combinational logic
     logic [ENTRY_WIDTH:0] num_entry_used; // can be 0,1,2,3,4, so ENTRY_WIDTH do not need to be decremented by 1
@@ -54,7 +62,9 @@ module reservation_station #(
     generate
         genvar i;
         for (i = 0; i < NUM_ENTRIES; i++) begin : check_ready
-        assign ready_flags[i] = entries[i].valid && entries[i].ready1 && entries[i].ready2;
+        assign ready_flags[i] = entries[i].valid 
+                                && (entries[i].ready1 || (wakeup && wakeup_tag_reg == entries[i].t1)) 
+                                && (entries[i].ready2 || (wakeup && wakeup_tag_reg == entries[i].t2));
         end
     endgenerate
 
@@ -79,46 +89,52 @@ module reservation_station #(
     // update issue
     // logic issue_internal;
 
+    RS_ENTRY empty_entry;
+    assign empty_entry = '{func: ALU_ADD, 
+                                t1: '0, 
+                                t2: '0, 
+                                v1: 1'b0, 
+                                v2: 1'b0, 
+                                dst: '0, 
+                                Bday: '0, 
+                                valid: 1'b0};
+
+    always_comb begin
+
+    end
+
 
     always_ff @(posedge clk) begin
         if (reset) begin
             for (int i = 0; i < NUM_ENTRIES; i++) begin
-                entries[i] <= '{insn: ALU_ADD, 
-                                inp1: '0, 
-                                inp2: '0, 
-                                ready1: 1'b0, 
-                                ready2: 1'b0, 
-                                dst: '0, 
-                                Bday: '0, 
-                                valid: 1'b0};
+                entries[i] <= empty_entry;
             end
             for (int i = 0; i < PHY_REG_NUM; i++) begin
                 ready_table[i] <= 1;
             end
-            insn_out <= 0;
-            inp1_out <= 0;
-            inp2_out <= 0;
-            dst_out <= 0;
+            func_out <= 0;
+            v1_out <= 0;
+            v2_out <= 0;
+            dst_tag <= 0;
         end else begin
             // add new instruction to reservation station
             if (load && ~is_full) begin
                 for (int i = 0; i < NUM_ENTRIES; i++) begin // it's for sure that an empty entry exists
                     if (!entries[i].valid) begin
-                        entries[i] <= '{insn: insn, 
-                                    inp1: inp1, 
-                                    inp2: inp2, 
-                                    ready1: ready_table[inp1], 
-                                    ready2: ready_table[inp2], 
+                        entries[i] <= '{func: func, 
+                                    t1: t1, 
+                                    t2: t2, 
                                     dst: dst, 
+                                    ready1: ready1 || (wakeup && wakeup_tag_reg == t1), 
+                                    ready2: ready2 || (wakeup && wakeup_tag_reg == t2), 
+                                    v1: (wakeup && wakeup_tag_reg == t1) ? wakeup_value_reg : v1,
+                                    v2: (wakeup && wakeup_tag_reg == t2) ? wakeup_value_reg : v2,
                                     Bday: (issue && exist_ready_out) ? num_entry_used - 1 : num_entry_used, 
                                     valid: 1};
                         break;
                     end else
                         ;
                 end
-
-                // update ready table
-                ready_table[dst] <= dst == 0; // reg0 should always be 0, so always ready
 
                 // we do not need to update exsiting valid entries whose inp1 == dst or inp2 ==dst
                 // namely ready1 and ready2 for existing entries do not need to be updated
@@ -131,10 +147,10 @@ module reservation_station #(
             if (issue) begin // it's for sure that exist_ready_out == 1, guaranteed by external issue unit
 
                 //output insn
-                insn_out <= entries[min_idx].insn;
-                inp1_out <= entries[min_idx].inp1;
-                inp2_out <= entries[min_idx].inp2;
-                dst_out <= entries[min_idx].dst;
+                func_out <= entries[min_idx].func; // output at clock edge just to make sure that RS output one single and stable value per clock cycle
+                v1_out <= (wakeup && wakeup_tag_reg == t1) ? wakeup_value_reg : entries[min_idx].v1;
+                v2_out <= (wakeup && wakeup_tag_reg == t2) ? entries[min_idx].v2;
+                dst_tag <= entries[min_idx].dst;
                 entries[min_idx].valid <= 0; 
 
                 // update Bday if it is younger than the output insn
@@ -149,10 +165,11 @@ module reservation_station #(
 
             // wakeup other insns if their inp is equal to the dst of the output insn
             if (wakeup) begin
-                ready_table[wakeup_reg_addr] <= 1;
                 for (int i = 0; i < NUM_ENTRIES; i++) begin
-                    entries[i].ready1 <= (entries[i].valid && entries[i].inp1 == wakeup_reg_addr) ? 1:entries[i].ready1;
-                    entries[i].ready2 <= (entries[i].valid && entries[i].inp2 == wakeup_reg_addr) ? 1:entries[i].ready2;
+                    entries[i].ready1 <= (entries[i].valid && entries[i].t1 == wakeup_tag_reg) ? 1:entries[i].ready1;
+                    entries[i].ready2 <= (entries[i].valid && entries[i].t2 == wakeup_tag_reg) ? 1:entries[i].ready2;
+                    entries[i].v1 <= (entries[i].valid && entries[i].t1 == wakeup_tag_reg) ? wakeup_value_reg:entries[i].v1;
+                    entries[i].v2 <= (entries[i].valid && entries[i].t2 == wakeup_tag_reg) ? wakeup_value_reg:entries[i].v2;
                 end  
             end else
                 ;              
