3d5799bf9d7621ab32daca052149ad05331f315b
jennielien <115868792+jennielien@users.noreply.github.com>
2024-07-10 21:25:45 +0800

    handling j-type

--- src/functional_unit/branch_target_unit.sv
+++ src/functional_unit/branch_target_unit.sv
@@ -1,14 +1,18 @@
 `include "sys_defs.svh"
 
 module branch_unit(
-    input [`XLEN-1:0] rs1, // value to check against branch condition
-	input [`XLEN-1:0] rs2,
-	input  [2:0] func, // specifies which condition to check
-	
-	input [`XLEN-1:0] pc, //target addr cal
-	input [`XLEN-1:0] imm, 
+    input logic b_j, // 0 for b-type ; 1 for j-type
+    input [`XLEN-1:0] pc, //target addr cal
+    input [`XLEN-1:0] imm,
 
-	output logic cond, // 0/1 condition result
+    // b-type
+    input [`XLEN-1:0] rs1, // also for jalr
+	input [`XLEN-1:0] rs2,
+	input  [2:0] func, // specifies which condition to check, for b-type
+	// j-type
+	input logic jal_jalr; // 0 for jal ; 1 for jalr
+
+	output logic cond, // 1 for misprediction/flush
 	output [`XLEN-1:0] target_pc
 );
     
@@ -17,24 +21,40 @@ module branch_unit(
 	assign signed_rs1 = rs1;
 	assign signed_rs2 = rs2;
 	always_comb begin
-		cond = 0;
-		unique case (func)
-			3'b000: cond = signed_rs1 == signed_rs2;  // BEQ
-			3'b001: cond = signed_rs1 != signed_rs2;  // BNE
-			3'b100: cond = signed_rs1 < signed_rs2;   // BLT
-			3'b101: cond = signed_rs1 >= signed_rs2;  // BGE
-			3'b110: cond = rs1 < rs2;                 // BLTU
-			3'b111: cond = rs1 >= rs2;                // BGEU
-			default: cond = 1'b0;
-		endcase
+	    if (b_j == 1'b0) begin
+            cond = 0;
+            unique case (func)
+                3'b000: cond = signed_rs1 == signed_rs2;  // BEQ
+                3'b001: cond = signed_rs1 != signed_rs2;  // BNE
+                3'b100: cond = signed_rs1 < signed_rs2;   // BLT
+                3'b101: cond = signed_rs1 >= signed_rs2;  // BGE
+                3'b110: cond = rs1 < rs2;                 // BLTU
+                3'b111: cond = rs1 >= rs2;                // BGEU
+                default: cond = 1'b0;
+            endcase
+        end
+        else begin
+            cond = 1;
+        end
 	end
 	
-	// brnach traget address calculation 
+	// traget address calculation 
+	logic [`XLEN-1:0] addr_src1, addr_src2;
 	assign addr_src1 = pc;
 	assign addr_src2 = imm;
 	
 	always_comb begin
-		target_pc = addr_src1 + addr_src2;
+	   if (b_j == 1'b0) begin
+		  target_pc = pc + imm;
+	   end
+	   else begin
+	       if (jal_jalr == 1'b0) begin
+		      target_pc = pc + imm;
+		   end
+		   else begin
+		      target_pc = rs1 + imm;
+		   end
+	   end
 	end
 	
 	/*
