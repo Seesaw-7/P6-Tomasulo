a172ecde97c325c17209370151e68381f671e64d
Seesaw <weiseesaw@gmail.com>
2024-07-10 14:47:35 +0200

    style: enum FUs

--- include/sys_defs.svh
+++ include/sys_defs.svh
@@ -19,9 +19,28 @@
 `define PHY_REG_ADDR_LEN         6 // physical regFile has 2^5 entries
 `define PHY_REG_NUM 			64
 
+
+`define XLEN 				32 // ISA bit length
+
+`define RS_INT_ENTRY_NUM 8 // #entries in RS, ahead of integer unit
+`define RS_INT_ENTRY_WIDTH 3 // #entries = 2^{ENTRY_WIDTH}
+
+`define RS_MULT_ENTRY_NUM 4 // #entries in RS, ahead of multiplier
+`define RS_MULT_ENTRY_WIDTH 2
+
+`define RS_BRANCH_ENTRY_NUM 4 // #entries in RS, ahead of branch unit
+`define RS_BRANCH_ENTRY_WIDTH 2
+
+`define RS_LS_ENTRY_NUM 8 // #entries in RS, ahead of load/store unit
+`define RS_LS_ENTRY_WIDTH 3
+
+`define ROB_SIZE 64
+`define ROB_TAG_LEN 6
+
+`define FU_NUM 4
+
 //
 // ALU function code input
-// probably want to leave these alone
 //
 typedef enum logic [4:0] {
 	ALU_ADD     = 5'h00,
@@ -44,22 +63,12 @@ typedef enum logic [4:0] {
 	// ALU_REMU    = 5'h11
 } ALU_FUNC;
 
-`define XLEN 				32 // ISA bit length
-
-`define RS_INT_ENTRY_NUM 8 // #entries in RS, ahead of integer unit
-`define RS_INT_ENTRY_WIDTH 3 // #entries = 2^{ENTRY_WIDTH}
-
-`define RS_MULT_ENTRY_NUM 4 // #entries in RS, ahead of multiplier
-`define RS_MULT_ENTRY_WIDTH 2
-
-`define RS_BRANCH_ENTRY_NUM 4 // #entries in RS, ahead of branch unit
-`define RS_BRANCH_ENTRY_WIDTH 2
-
-`define RS_LS_ENTRY_NUM 8 // #entries in RS, ahead of load/store unit
-`define RS_LS_ENTRY_WIDTH 3
-
-`define ROB_SIZE 64
-`define ROB_TAG_LEN 6
+typedef enum logic [1:0] {
+	FU_LSU    		= 2'b00, // load/store unit
+	FU_MULT     	= 2'b01, // multiplier
+	FU_BTU     		= 2'b10, // branch target unit
+	FU_ALU     		= 2'b11, // arithmetic and logic unit
+} FUNC_UNIT;
 
 typedef struct packed {
     logic unsigned [`REG_ADDR_LEN-1:0] src1;
