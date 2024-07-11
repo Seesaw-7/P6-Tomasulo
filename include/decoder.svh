aebf2367c2a8acd0a0ade8d2abf0ddd01b4f31e4
jennielien <115868792+jennielien@users.noreply.github.com>
2024-07-11 12:49:49 +0800

    Update decoder.svh

--- include/decoder.svh
+++ include/decoder.svh
@@ -1,20 +1,17 @@
-`ifndef __REORDER_BUFFER_VH__
-`define __REORDER_BUFFER_VH__
-
-`define XLEN 32
-
-`define REG_LEN 32
-`define REG_ADDR_LEN 5
-
-// `define ROB_SIZE 64
-// `define ROB_TAG_LEN 6
+`ifndef __DECODER_SVH__
+`define __DECODER_SVH__
 
 typedef struct packed {
-    logic valid;
-    logic ready;
-    logic mispredict;
-    logic unsigned [`REG_ADDR_LEN-1:0] wb_reg;
-    logic [`XLEN-1:0] wb_data;
-} ROB_ENTRY;
+	logic unsigned valid;
+	FUNC_UNIT fu; // 4 kinds of FU in enum
+	ARCH_REG arch_reg; // TODO: 5'b0 if empty, especially lw sw
+	logic [`XLEN-1:0] imm; // TODO: immediate or offest; f empty: 0
+	ALU_FUNC alu_func;
+	// logic rd_mem;
+	// logic wr_mem;
+	//logic cond_branch;
+	//logic uncond_branch; 
+	logic [`XLEN-1:0] pc;
+} DECODED_PACK;
 
 `endif
