544b6ea8aa3dd75ea4249d82777b6c7ae8941659
jennielien <115868792+jennielien@users.noreply.github.com>
2024-07-12 00:13:04 +0800

    update input port

--- src/reorder_buffer.sv
+++ src/reorder_buffer.sv
@@ -1,100 +1,21 @@
-`include "reorder_buffer.svh"
-`include "sys_defs.svh"
+`ifndef __REORDER_BUFFER_VH__
+`define __REORDER_BUFFER_VH__
 
-module reorder_buffer(
-    input logic clk,
-    input logic reset,
-    
-    input logic dispatch,
-    input [`REG_ADDR_LEN-1:0] reg_addr_from_dispatcher,
-     
-    input logic cdb_to_rob,
-    input [`ROB_TAG_LEN-1:0] rob_tag_from_cdb,
-    input [`XLEN-1:0] cdb_result,
-    input logic mispredict_from_cdb,
-    
-    output logic wb_en, //wb to reg
-    output [`REG_ADDR_LEN-1:0] wb_reg, 
-    output [`XLEN-1:0] wb_data,
-    
-    output logic flush,
-    
-    output [`ROB_TAG_LEN-1:0] assign_rob_tag_to_dispatcher,
-    output logic rob_full_adv 
-    
-    //rob_curr output
-);
-    
-    ROB_ENTRY rob_curr [`ROB_SIZE-1:0];
-    ROB_ENTRY rob_next [`ROB_SIZE-1:0];
+`define XLEN 32
 
-    logic [`ROB_TAG_LEN-1:0] head_curr;
-    logic [`ROB_TAG_LEN-1:0] head_next;
-    logic [`ROB_TAG_LEN-1:0] tail_curr;
-    logic [`ROB_TAG_LEN-1:0] tail_next;
-    
-    always_comb begin
-        for (int i=0; i<`ROB_SIZE; i++) begin
-            rob_next[i] = rob_curr[i];
-        end
-        head_next = head_curr;
-        tail_next = tail_curr;
-        
-        wb_en = 1'b0;
-        wb_reg = {`REG_ADDR_LEN{1'b0}};
-        wb_data = {`XLEN{1'b0}};
-        flush = 1'b0;
-        assign_rob_tag_to_dispatcher = {`ROB_TAG_LEN{1'b0}};
-        
-        if (dispatch) begin
-            rob_next[tail_curr].valid = 1'b1;
-            rob_next[tail_curr].ready = 1'b0;
-            rob_next[tail_curr].mispredict = 1'b0;
-            rob_next[tail_curr].wb_reg = reg_addr_from_dispatcher;
-            rob_next[tail_curr].wb_data = {`XLEN{1'b0}};
-            tail_next = (tail_curr + 1) % `ROB_SIZE;
-            
-            assign_rob_tag_to_dispatcher = tail_curr;
-        end
-        
-        if (cdb_to_rob) begin
-            rob_next[rob_tag_from_cdb].wb_data = cdb_result;
-            rob_next[rob_tag_from_cdb].mispredict = mispredict_from_cdb;
-            rob_next[rob_tag_from_cdb].ready = 1'b1;
-        end
-        
-        if (rob_curr[head_curr].valid && rob_curr[head_curr].ready) begin
-            wb_en = 1'b1;
-            wb_reg = rob_curr[head_curr].wb_reg;
-            wb_data = rob_curr[head_curr].wb_data;
-            if (rob_curr[head_curr].mispredict == 1'b1) begin
-                flush = 1'b1;
-            end
-            rob_next[head_curr].valid = 1'b0;
-            head_next = (head_curr + 1) % `ROB_SIZE;
-        end
+`define REG_LEN 32
+`define REG_ADDR_LEN 5
 
-    end
+// `define ROB_SIZE 64
+// `define ROB_TAG_LEN 6
 
-    assign rob_full_adv = (head_curr == tail_next);
-    
-    always_ff @(posedge clk) begin
-        if (reset || flush) begin
-            head_curr <= {`REG_ADDR_LEN{1'b0}};
-            tail_curr <= {`REG_ADDR_LEN{1'b0}};
-            for (int i=0; i<`ROB_SIZE; i++) begin
-                rob_curr[i].valid <= 1'b0;
-                rob_curr[i].ready <= 1'b0; 
-                rob_curr[i].mispredict <= 1'b0;
-                rob_curr[i].wb_reg <= {`REG_ADDR_LEN{1'b0}};
-                rob_curr[i].wb_data <= {`XLEN{1'b0}};
-            end
-        end
-        else begin
-            rob_curr <= rob_next;
-            head_curr <= head_next;
-            tail_curr <= tail_next;
-        end
-    end
-    
-endmodule
+typedef struct packed {
+    logic valid;
+    logic ready;
+    logic mispredict;
+    logic unsigned [`REG_ADDR_LEN-1:0] wb_reg;
+    logic [`XLEN-1:0] wb_data;
+    logic [`XLEN-1:0] target_pc;
+} ROB_ENTRY;
+
+`endif
