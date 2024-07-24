8acc644c0b52016e73be6c87965c18b70258aea3
Specter <yizhiy@kth.se>
2024-07-24 15:10:27 -0400

    new rs

--- src/reservation_station.sv
+++ src/reservation_station.sv
@@ -8,6 +8,145 @@
 // Note: this module assumes that wakeup is stable at posedge, and wakeup is one clock behind issue
 // Besides, it assumes that wakeup_tag and wakeup_value are stable at clock edge, which is kind of weird
 
+
+module reservation_station #(
+    parameter NUM_ENTRIES = 8, // #entries in the reservation station
+    parameter ENTRY_WIDTH = 3  // #entries = 2^{ENTRY_WIDTH}
+) (
+    // control signals
+    input clk,
+    input reset,
+
+    // from dispatch
+    input logic load,
+    input INST_RS insn_load,
+
+    // from FU reg 
+    input unsigned [3:0] wakeup ,
+    input [3:0] [`XLEN-1:0] wakeup_value,
+    input [3:0] [`ROB_TAG_LEN-1:0] wakeup_tag, // tag already executed
+    // input [3:0] [`ROB_TAG_LEN-1:0] ex_tag, 
+    // wakeup_tag under execution (before or after fu based on issue)
+
+    // from issue unit
+    input unsigned clear,
+    input [`ROB_TAG_LEN-1:0] clear_tag,
+
+    output RS_ENTRY insn_for_ex, // not always ready & reg
+    output logic is_full
+);
+
+    RS_ENTRY issue_queue_curr [NUM_ENTRIES]; 
+    RS_ENTRY issue_queue_next [NUM_ENTRIES]; 
+    RS_ENTRY insn_for_ex_next; // not always ready & reg
+
+    always_ff @(posedge clk) begin
+        unique if (reset) begin
+            issue_queue_curr <= 0;
+            insn_for_ex <= 0;
+        end else begin
+            issue_queue_curr <= issue_queue_next;
+            insn_for_ex <= insn_for_ex_next;
+        end
+    end
+
+    always_comb begin
+        issue_queue_next = issue_queue_curr;
+        // load insn from dispatcher
+        if (load) begin
+            issue_queue_next[NUM_ENTRIES-1].insn = insn_load;
+            issue_queue_next[NUM_ENTRIES-1].valid = 1;
+        // clear insn
+        if (clear) begin
+            for (int i=0; i<ENTRY_NUM; ++i) begin
+                if (issue_queue_next[i].insn.insn_tag == clear_tag) begin
+                    issue_queue_next[i].valid = 0;
+                end
+            end
+        end
+        // move everything to the end
+        for (int i=0; i<NUM_ENTRIES-1; ++i) begin // TODO: test whether parallel
+            if (!issue_queue_next[i].valid) begin
+                issue_queue_next[i] = issue_queue_next[i+1];
+            end
+        end
+        // wakeup
+        for (int j=0; j<4; ++j) begin
+           if (wakeup[j]) begin
+                for (int i=0; i<NUM_ENTRIES; ++i) begin
+                    if (issue_queue_next[i].tag_src1 == wakeup_tag[j]) begin
+                        issue_queue_next[i].value_src1 = wakeup_value[j];
+                        issue_queue_next[i].ready_src1 = 1;
+                    end
+                    if (issue_queue_next[i].tag_src2 == wakeup_tag[j]) begin
+                        issue_queue_next[i].value_src2 = wakeup_value[j];
+                        issue_queue_next[i].ready_src2 = 1;
+                    end
+                end 
+            end
+        end
+    end
+
+    RS_ENTRY insn_ready, insn_to_ready;
+    always_comb begin
+        insn_ready = 0;
+        insn_to_ready = 0;
+        // TODO: check whether this for loop siquentially assign variables
+        // find the first ready insn
+        for (int i=NUM_ENTRIES; i>=0; --i) begin
+            unique if (issue_queue_next[i].valid && issue_queue_next[i].insn.ready_rs1 && issue_queue_next[i].insn.ready_rs2) begin
+                insn_ready = issue_queue_next[i];
+            end else begin
+                insn_ready = insn_ready;
+            end
+        end
+        // find the first ready insn with the fast fu
+        /*
+            for i <- 0 : NUM_ENTRIES-1
+                if valid 
+                    for j <- flip [FU_LSU, FU_MULT, FU_BTU, FU_ALU]
+                        if !wakeup[j] -- not ready 
+                            flag <- check whether add wake_up tag will be ready
+                            if (src1 ready && tag2 == wakeup_tag[j]) or (src2 ready && tag2 == wakeup_tag[j]) or (tag1 == tag2 == wakeup_tag[j]) -- we don't care about two src with different tag ready together in the future
+                                insn_to_ready = current insn
+                                break
+                else
+                    break
+        */ 
+        for (int i=NUM_ENTRIES; i>=0; --i) begin
+            unique if (issue_queue_next[i].valid) begin
+                for (int j=0; j<4; ++j) begin //  j follows order [FU_LSU, FU_MULT, FU_BTU, FU_ALU]
+                    unique if (wake_up[j]) begin
+                        insn_to_ready = insn_to_ready;
+                    end else begin
+                        unique if (
+                            (issue_queue_next[i].insn.ready_src1 && (issue_queue_next[i].insn.tag_src2 == wakeup_tag[j])) ||
+                            (issue_queue_next[i].insn.ready_src2 && (issue_queue_next[i].insn.tag_src1 == wakeup_tag[j])) ||
+                            ((issue_queue_next[i].insn.tag_src1 == wakeup_tag[j]) && (issue_queue_next[i].insn.tag_src2 == wakeup_tag[j]))
+                        ) begin
+                           insn_to_ready = issue_queue_next[i]; 
+                        end else begin
+                            insn_to_ready = insn_to_ready;
+                        end
+                    end
+                end
+            end else begin
+                insn_to_ready = insn_to_ready;
+            end
+        end
+
+        unique if (insn_ready.insn.valid) begin
+            insn_for_ex_next = insn_ready;
+        end else begin
+            insn_for_ex_next = insn_to_ready;
+        end
+    end
+
+    assign is_full =  issue_queue_next[NUM_ENTRIES-1].valid;
+    
+endmodule
+
+/*
 module reservation_station #(
     parameter NUM_ENTRIES = 8, // #entries in the reservation station
     parameter ENTRY_WIDTH = 3  // #entries = 2^{ENTRY_WIDTH}
@@ -189,3 +328,4 @@ module reservation_station #(
 
 
 endmodule
+*/
