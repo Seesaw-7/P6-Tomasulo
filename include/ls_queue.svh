ae8692a973b6e810b71463083bb4db12da52647e
jennielien <115868792+jennielien@users.noreply.github.com>
2024-07-29 23:54:20 +0800

    fix typo

--- include/ls_queue.svh
+++ include/ls_queue.svh
@@ -11,12 +11,12 @@ typedef struct packed {
     logic valid; 
     logic ready; 
     logic read_write; // read:1 write:0
-    logic INST_RS insn; 
+    INST_RS insn; 
 } LS_QUEUE_ENTRY;
 
 typedef struct packed {
     logic read_write; 
-    logic INST_RS insn; 
+    INST_RS insn; 
 } LS_UNIT_PACK;
 
 `endif
