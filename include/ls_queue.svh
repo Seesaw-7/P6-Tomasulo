9570b99c6f4d7339bd4442e884249026526fd751
jennielien <115868792+jennielien@users.noreply.github.com>
2024-07-29 10:45:15 +0800

    upload ls_queue.svh

---
+++ include/ls_queue.svh
@@ -0,0 +1,22 @@
+`ifndef __LS_QUEUE_SVH__
+`define __LS_QUEUE_SVH__
+
+`include "sys_defs.svh"
+`include "dispatcher.svh"
+
+`define LS_QUEUE_SIZE 8
+`define LS_QUEUE_POINTER_LEN 3
+
+typedef struct packed {
+    logic valid; 
+    logic ready; 
+    logic read_write; // read:1 write:0
+    logic INST_RS insn; 
+} LS_QUEUE_ENTRY;
+
+typedef struct packed {
+    logic read_write; 
+    logic INST_RS insn; 
+} LS_UNIT_PACK;
+
+`endif
