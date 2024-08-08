22f002e67b60689e0e931a5b56ccb66c00128538
SyjEmma <156131433+SyjEmma@users.noreply.github.com>
2024-08-08 14:47:18 +0800

    update ls

--- src/functional_unit/ls_unit.sv
+++ src/functional_unit/ls_unit.sv
@@ -5,17 +5,14 @@
 `include "ls_queue.svh"
 
 module ls_unit(
-    // input [`XLEN-1:0] opa,
-    // input [`XLEN-1:0] opb, //Iimm for LW, Simm for SW
     input LS_UNIT_PACK insn_in, 
     input logic en, 
     input logic mem_hit, // from mem; hit or miss
     input [`XLEN-1:0] load_data, // load from mem
 
-    output logic mem_read,
-    output logic mem_write, 
+    output BUS_COMMAND mem_command,
     output logic [`XLEN-1:0] mem_addr, 
-    output logic [2:0] mem_size,
+    output MEM_SIZE mem_size,
     output logic [`XLEN-1:0] proc2Dmem_data,
     output logic [`XLEN-1:0] wb_data, // load from mem
     output logic [`ROB_TAG_LEN-1:0] inst_tag, 
@@ -23,20 +20,28 @@ module ls_unit(
 );
    
     logic [`XLEN-1:0] data_from_mem, unsigned_result;  
-    logic signed signed_result;
+    logic signed [`XLEN-1:0] signed_result;
     
     always_comb begin
+        // Default values
+        mem_command = BUS_NONE;
+        mem_addr = '0;
+        mem_size = BYTE; // Default to BYTE, adjust as needed
+        proc2Dmem_data = '0;
+        wb_data = '0;
+        inst_tag = '0;
+        done = 1'b0;
+
         if (en == 1'b1) begin
             mem_addr = insn_in.insn.value_src1 + insn_in.insn.imm;
-            mem_size = insn_in.insn.func3; 
+            mem_size = MEM_SIZE'(insn_in.insn.func3); 
+            mem_command = insn_in.mem_command; // Use the mem_command from insn_in
             
             // load
-            if (insn_in.read_write == 1'b1) begin
-                mem_read = 1'b1;
-                mem_write = 1'b0;
+            if (insn_in.mem_command == BUS_LOAD) begin
                 if (mem_hit == 1'b1) begin
                     data_from_mem = load_data; 
-                    signed_result = $signed(data_from_mem); // 32'(signed'(data_from_mem)) 
+                    signed_result = $signed(data_from_mem);
                     unsigned_result = data_from_mem;
                     if (insn_in.insn.func == LS_LOAD) begin
                         wb_data = signed_result;
@@ -50,9 +55,7 @@ module ls_unit(
                 end
             end
             // store
-            else begin
-                mem_read = 1'b0;
-                mem_write = 1'b1;
+            else if (insn_in.mem_command == BUS_STORE) begin
                 proc2Dmem_data = insn_in.insn.value_src2; 
             end
             done = mem_hit;
