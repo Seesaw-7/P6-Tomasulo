f031b454326af0f3a3ed955c1112bdcbeeca08e1
jennielien <115868792+jennielien@users.noreply.github.com>
2024-08-08 22:17:31 +0800

    Update common_data_bus.sv

--- src/common_data_bus.sv
+++ src/common_data_bus.sv
@@ -1,22 +1,10 @@
+/////////////////////////////////////////////////////////////////////////
+// Module name : common_data_bus.sv
+// Description : This module handles the communication between FU reg and ROB. 
+/////////////////////////////////////////////////////////////////////////
 `timescale 1ns/1ps
 
 `include "sys_defs.svh"
-
-// P6 style Description: 
-// The CDB (Common Data Bus) module selects and broadcasts results from functional units (FUs) to 
-// the reorder buffer (ROB) and reservation station. It takes inputs from functional units and the issue unit, 
-// selects the appropriate result based on a select signal, and outputs the result to the reservation station and ROB.
-
-// Inputs:
-// - in_values ([`XLEN-1:0] [FU_NUM-1:0]): Array of results from functional units.
-// - ROB_tag嚗ypass to ROB, forward to RS
-// - select_flag (logic): Indicates if selection is valid, bypass to ROB
-// - select_signal (logic [$clog2(FU_NUM)-1:0]): signal selecting which result to output.
-
-// Outputs:
-// - out_select_flag (logic): Passes through the input select flag.
-// - out_ROB_tag (logic [4:0]): Passes through the input ROB tag, bypass to ROB, forward to RS
-// - out_value (logic [`XLEN-1:0]): Value output to the ROB, forward to RS.
 
 module common_data_bus (
     input [3:0] [`XLEN-1:0] fu_results,
